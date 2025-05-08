d598c41dabc14ecf3bdc4bc4ce7c06b0eb42b302
Gabikoski <51301595+Gabikoski@users.noreply.github.com>
2025-05-08 15:42:05 -0300

    update lesson.js

--- lesson.js
+++ lesson.js
@@ -7,3 +7,33 @@ function Somar(){
 
     console.log(`Soma ${valor1}+${valor2}=${resultado}`);
 }
+
+function Subtrair(){
+    let valor1 = 0;
+    let valor2 = 0;
+    valor1 = Number(prompt("Valor 1:"));
+    valor2 = Number(prompt("Valor 2:"));
+    let resultado = valor1 - valor2;
+
+    console.log(`Subtracao ${valor1}-${valor2}=${resultado}`); 
+}
+
+function Multiplicar(){
+    let valor1 = 0;
+    let valor2 = 0;
+    valor1 = Number(prompt("Valor 1:"));
+    valor2 = Number(prompt("Valor 2:"));
+    let resultado = valor1 * valor2;
+
+    console.log(`Multiplicação ${valor1}*${valor2}=${resultado}`); 
+}
+
+function Dividir(){
+    let valor1 = 0;
+    let valor2 = 0;
+    valor1 = Number(prompt("Valor 1:"));
+    valor2 = Number(prompt("Valor 2:"));
+    let resultado = valor1 / valor2;
+
+    console.log(`Divisao ${valor1}/${valor2}=${resultado}`); 
+}
